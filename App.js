c0e36558c37218bfb95a6e6568de4cb15caf7a72
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-07-08 17:43:56 +0900

    feat: flex box styling practice

--- App.js
+++ App.js
@@ -1,26 +1,45 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.dummyText}>올 여름 펜타포트!!!</Text>
-      <Text style={styles.dummyText}>타임 테이블 앱 만들어보자고!!!</Text>
-      <Button title="@fstval.life" />
+    <View style={styles.abbContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="당신의 목표를 입력하세요!"
+        />
+        <Button title="Add goal!!" />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of Goal...</Text>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  abbContainer: {
+    paddingTop: 50,
+    paddingHorizontal: 16,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  dummyText: {
-    margin: 16,
-    padding: 5,
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    gap: 8,
+    flex: 1,
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  textInput: {
     borderWidth: 2,
-    borderColor: 'green',
+    borderColor: '#cccccc',
+    width: '70%',
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 })
