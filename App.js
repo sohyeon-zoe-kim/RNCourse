bd48a14d6263163001a0e5309d1935da9df1452e
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-07-08 17:54:54 +0900

    feat: useState로 courseGoals 상태 관리 로직 추가

--- App.js
+++ App.js
@@ -1,17 +1,35 @@
+import { useState } from 'react'
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('')
+  const [courseGoals, setCourseGoals] = useState([])
+
+  const goalInputHandler = enteredText => {
+    setEnteredGoalText(enteredText)
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentCourseGoals => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ])
+  }
+
   return (
     <View style={styles.abbContainer}>
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.textInput}
           placeholder="당신의 목표를 입력하세요!"
+          onChangeText={goalInputHandler}
         />
-        <Button title="Add goal!!" />
+        <Button title="Add goal!!" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of Goal...</Text>
+        {courseGoals.map(goal => (
+          <Text key={goal}>{goal}</Text>
+        ))}
       </View>
     </View>
   )
