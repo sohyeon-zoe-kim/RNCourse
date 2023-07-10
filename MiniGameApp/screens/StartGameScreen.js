4c398ed1f82bc31d9335d54401f8a75b8846c3be
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-07-10 22:31:45 +0900

    feat: NumberInput 스타일링 및 TextInput 필드 구성

--- MiniGameApp/screens/StartGameScreen.js
+++ MiniGameApp/screens/StartGameScreen.js
@@ -5,7 +5,13 @@ import PrimaryButton from '../components/PrimaryButton'
 function StartGameScreen() {
   return (
     <View style={style.inputContainer}>
-      <TextInput />
+      <TextInput
+        style={style.numberinput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -26,5 +32,16 @@ const style = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  numberinput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: '#ffffff',
+    borderBottomWidth: 2,
+    color: '#ffffff',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 })
