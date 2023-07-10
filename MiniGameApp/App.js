ddc54c90bdb28e1cfdc10cb4f60887691b2f369a
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-07-10 23:27:02 +0900

    feat: imagebackground 추가

--- MiniGameApp/App.js
+++ MiniGameApp/App.js
@@ -1,12 +1,19 @@
 import { LinearGradient } from 'expo-linear-gradient'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, ImageBackground } from 'react-native'
 
 import StartGameScreen from './screens/StartGameScreen'
 
 export default function App() {
   return (
     <LinearGradient colors={['#ff4463', '#e5ff7e']} style={styles.rootScreen}>
-      <StartGameScreen />
+      <ImageBackground
+        source={require('./assets/images/background.png')}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   )
 }
@@ -14,5 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 })
