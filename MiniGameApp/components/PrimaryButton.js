b3d33ac8888e68e75ca4402e9645eb33a8c65a9d
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-07-10 22:57:12 +0900

    feat: 버튼에 시각적 피드백 추가

--- MiniGameApp/components/PrimaryButton.js
+++ MiniGameApp/components/PrimaryButton.js
@@ -1,11 +1,47 @@
-import { View, Text } from 'react-native'
+import { StyleSheet, View, Text, Pressable } from 'react-native'
 
 function PrimaryButton({ children }) {
+  function pressHandler() {
+    console.log('pressed!')
+  }
+
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: '#d3475e' }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   )
 }
 
 export default PrimaryButton
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  buttonInnerContainer: {
+    backgroundColor: '#ff6f87',
+    borderRadius: 28,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+})
