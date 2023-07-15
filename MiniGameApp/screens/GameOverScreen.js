1e7990f842f3dd5fef16a4c43eb419f641406444
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-07-15 22:05:18 +0900

    feat: GameOverScreen에 전경 이미지 추가

--- MiniGameApp/screens/GameOverScreen.js
+++ MiniGameApp/screens/GameOverScreen.js
@@ -1,13 +1,40 @@
-import { TextInput, View, Text } from 'react-native'
+import { StyleSheet, View, Image, Text } from 'react-native'
+
+import Title from '../components/ui/Title'
+import Colors from '../constants/colors'
 
 function StartGameScreen() {
   return (
-    <View>
-      <Text>Gmae Over!</Text>
-      <TextInput />
-      {/* //Button */}
+    <View style={styles.rootContainer}>
+      <Title>Game Over!</Title>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={require('../assets/images/success.png')} />
+      </View>
+      <Text>Your phone neede X rounds o guess the number Y</Text>
     </View>
   )
 }
 
 export default StartGameScreen
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.secondary500,
+    overflow: 'hidden',
+    margin: 36,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+})
