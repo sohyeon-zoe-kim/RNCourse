f566d2bf11882e766f7d14ce703326e8b33fb828
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-07-17 22:46:16 +0900

    feat: Redux Slices로 작업하기

--- FoodNavigationApp/store/redux/store.js
+++ FoodNavigationApp/store/redux/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import favoritesReducer from './favorite'
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    favoriteMeals: favoritesReducer,
+  },
 })
