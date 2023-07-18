d894e4cac978537bcd47784fc579942206647787
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-07-18 16:31:49 +0900

    feat: navigation.goBack() 추가하여 모달 닫기

--- AccountBookApp/screens/ManageExpense.js
+++ AccountBookApp/screens/ManageExpense.js
@@ -15,11 +15,17 @@ function ManageExpense({ route, navigation }) {
     })
   }, [navigation, isEditing])
 
-  function deleteExpenseHandler() {}
+  function deleteExpenseHandler() {
+    navigation.goBack()
+  }
 
-  function calcelHandler() {}
+  function calcelHandler() {
+    navigation.goBack()
+  }
 
-  function confirmHandler() {}
+  function confirmHandler() {
+    navigation.goBack()
+  }
 
   return (
     <View style={styles.container}>
