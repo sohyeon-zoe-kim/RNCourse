84b00897625444d1ff4848d44a60e199b93a265c
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-07-18 10:52:41 +0900

    feat: 지출에 관련된 컴포넌트 작업 계속해서 이어가기

--- AccountBookApp/components/ExpensesOutput/ExpensesOutput.js
+++ AccountBookApp/components/ExpensesOutput/ExpensesOutput.js
@@ -3,10 +3,10 @@ import { View } from 'react-native'
 import ExpensesList from './ExpensesList'
 import ExpensesSummary from './ExpensesSummary'
 
-function ExpensesOutput({ expenses }) {
+function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View>
-      <ExpensesSummary />
+      <ExpensesSummary periodName={expensesPeriod} expenses={expenses} />
       <ExpensesList />
     </View>
   )
