36f686c1dfa24265e0e3ca758d8544fd6ce8aa2d
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-07-18 11:02:08 +0900

    feat: 지출 리스트 출력하기

--- AccountBookApp/components/ExpensesOutput/ExpensesList.js
+++ AccountBookApp/components/ExpensesOutput/ExpensesList.js
@@ -1,7 +1,18 @@
-import { FlatList } from 'react-native'
+import { FlatList, Text } from 'react-native'
+
+function renderExpenseItem(itemData) {
+  const item = itemData.item
+  return <Text>{item.description}</Text>
+}
 
 function ExpensesList({ expenses }) {
-  return <FlatList />
+  return (
+    <FlatList
+      data={expenses}
+      renderItem={renderExpenseItem}
+      keyExtractor={item => item.id}
+    />
+  )
 }
 
 export default ExpensesList
