f2a11b13ee68f32b0325e6fb6d5d7fb4f5635a14
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-07-18 16:56:52 +0900

    feat: context 상태 관리 추가

--- AccountBookApp/components/ExpensesOutput/ExpensesOutput.js
+++ AccountBookApp/components/ExpensesOutput/ExpensesOutput.js
@@ -4,62 +4,6 @@ import ExpensesList from './ExpensesList'
 import ExpensesSummary from './ExpensesSummary'
 import { GlobalStyles } from '../../constants/styles'
 
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    description: 'A pair of shoes',
-    amount: 59.99,
-    date: new Date('2023-07-18'),
-  },
-  {
-    id: 'e2',
-    description: 'A pair of trousers',
-    amount: 89.29,
-    date: new Date('2023-07-21'),
-  },
-  {
-    id: 'e3',
-    description: 'Some bananas',
-    amount: 5.99,
-    date: new Date('2023-09-02'),
-  },
-  {
-    id: 'e4',
-    description: 'A book',
-    amount: 14.99,
-    date: new Date('2023-09-03'),
-  },
-  {
-    id: 'e5',
-    description: 'Another book',
-    amount: 18.59,
-    date: new Date('2023-10-25'),
-  },
-  {
-    id: 'e6',
-    description: 'Another book',
-    amount: 18.59,
-    date: new Date('2023-10-25'),
-  },
-  {
-    id: 'e7',
-    description: 'Another book',
-    amount: 18.59,
-    date: new Date('2023-10-25'),
-  },
-  {
-    id: 'e8',
-    description: 'Another book',
-    amount: 18.59,
-    date: new Date('2023-10-25'),
-  },
-  {
-    id: 'e9',
-    description: 'Another book',
-    amount: 18.59,
-    date: new Date('2023-10-25'),
-  },
-]
 
 function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
